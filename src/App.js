459a08251cf8030cb4b6402a15089159f057a65d
Yatin Relhan <yrelhan4@gmail.com>
2020-05-21 19:01:25 -0500

    Homepage and login frontend added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
-
-import HomePage from './homepage.component';
+import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className='App'>
-        <HomePage />
-      </div>
-    );
-  }
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import Header from './components/header/header.component';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
